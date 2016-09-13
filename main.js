0d4adc08ead17f6abf92a1d139fb7c55638c9290
Luis Pérez <lapv1992@icloud.com>
2016-09-13 12:35:02 -0400

    Segundo Ejemplo

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!")
+var fs = require("fs");
+
+fs.readFile('text.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("Program Ended");
